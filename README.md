fcb9fbdf745bccfcf25bf89876d2810bb39f6d25
PhilKu7 <86799686+PhilKu7@users.noreply.github.com>
2023-12-12 22:49:17 +0100

    First test with Fourier Transformation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This repository contains the code to make a MKS Gen_L V1.0 steer an LED strip.
+
+This Website helped a lot with the fourier transformation: [medium.com](https://medium.com/@yolandaluqueh/music-to-led-strip-tutorial-using-fourier-transform-3d203a48fe14)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+// first include a library for fourier transform
+#include <arduinoFFT.h>
 
 #define MOSFET_E0 10
 #define MOSFET_E1 7
@@ -13,6 +15,19 @@
 #define PIN_MIC TEMP_0
 #define PIN_MIC_D Z_MAX
 
+#define SAMPLES 64              // Must be a power of 2
+#define SAMPLING_FREQUENCY 1000 // Hz, must be less than 10000 due to ADC
+
+arduinoFFT FFT = arduinoFFT(); // Create an FFT object
+
+unsigned int sampling_period_us; // Sampling period in microseconds
+unsigned long microseconds;      // microseconds since program start
+double vReal[SAMPLES];           // Create an array of size SAMPLES to hold real values
+double vImag[SAMPLES];           // Create an array of size SAMPLES to hold imaginary values
+
+float modules[3] = {0}; // This is where we're going to store the modules of the fourier transform
+int RGB_values[3] = {0}; // This is where we're going to store the RGB values
+
 // Number of samples to average
 const int numSamples = 100;
 
@@ -23,6 +38,7 @@ void setup()
 {
   // Initialize serial communication
   Serial.begin(115200);
+  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
   // Set the LED pin as an output
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIN_RED, OUTPUT);
@@ -34,52 +50,80 @@ void setup()
 
 void loop()
 {
-  int sound_level = 0;          // This is where we're going to store the raw analog value
-  int change[numSamples] = {0}; // This is where we're going to store the derivative of the sound level
-  float average_change = 0;       // This is where we're going to store the average change of the sound level
-  int brightness = 0;           // This is where we're going to store the brightness of the LED
-
-  // Then measure the sound level
-  sound_level = analogRead(PIN_MIC); // Read the analog value of the microphone
-
-  // Save the sound value to the circular buffer
-  for (int i = numSamples - 1; i > 0; i--)
+  for (int i = 0; i < SAMPLES; i++)
   {
-    micBuffer[i] = micBuffer[i - 1];
+    microseconds = micros(); // Overflows after around 70 minutes! (4294967295 us)
+    vReal[i] = analogRead(PIN_MIC);
+    vImag[i] = 0;
+    while (micros() < (microseconds + sampling_period_us))
+    {
+    } // empty loop
   }
-  micBuffer[0] = sound_level;
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
 
-  // Calculate each change of the sound level
-  for (int i = 0; i < numSamples - 1; i++)
+  for (int i = 0; i < 3; i++)
   {
-    change[i] = micBuffer[i] - micBuffer[i + 1];
+    modules[i] = 0;
+    RGB_values[i] = 0;
   }
-  // Calculate the average change of the sound level
-  for (int i = 0; i < numSamples - 1; i++)
-  {
-    average_change += change[i];
-  }
-  average_change /= numSamples;
-  if (average_change*200 > 6)
-  {
-    brightness = average_change*200;
-  }
-  else
-  {
-    brightness = 0;
-  }
-  // Set brightness of the LEDs
-  analogWrite(PIN_RED, brightness);
-  analogWrite(PIN_GREEN, brightness);
-  analogWrite(PIN_BLUE, brightness);
 
-  // Print the sound level to the Serial Monitor
-  Serial.print(">Sound:");
-  Serial.println(sound_level);
+  for (int i = 1; i < SAMPLES / 2; i++)
+  {
+    double a = pow(vReal[i], 2);
+    double b = pow(vImag[i], 2);
+    if (i <= SAMPLES / 6)
+    {
+      modules[0] += sqrt(a + b);
+    }
+    else if (i <= SAMPLES / 3)
+    {
+      modules[1] += sqrt(a + b);
+    }
+    else
+    {
+      modules[2] += sqrt(a + b);
+    }
+  }
 
-  // Print the brightness to the Serial Monitor
-  Serial.print(">Brightness:");
-  Serial.println(brightness);
-  // wait 100 microseconds before the next loop
-  delayMicroseconds(100);
+  for (int i = 0; i < 3; i++)
+  {
+    modules[i] /= SAMPLES / 6;
+  }
+
+  // convert modules to RGB values
+  RGB_values[0] = modules[0] /= 100;
+  RGB_values[1] = modules[1] * 2;
+  RGB_values[2] = modules[2] * 2;
+
+  for (int i = 0; i < 3; i++)
+  {
+    RGB_values[i] = modules[i];
+  }
+
+  Serial.print(">Red:");
+  Serial.println(RGB_values[0]);
+  Serial.print(">Green:");
+  Serial.println(RGB_values[1]);
+  Serial.print(">Blue:");
+  Serial.println(RGB_values[2]);
+
+  // limit all the brightnesses between 0 and 255
+  for (int i = 0; i < 3; i++)
+  {
+    if (RGB_values[i] > 255)
+    {
+      RGB_values[i] = 255;
+    }
+    else if (RGB_values[i] < 0)
+    {
+      RGB_values[i] = 0;
+    }
+  }
+  analogWrite(PIN_RED, RGB_values[0]);
+  analogWrite(PIN_GREEN, RGB_values[1]);
+  analogWrite(PIN_BLUE, RGB_values[2]);
+
+  delay(25);
 }
