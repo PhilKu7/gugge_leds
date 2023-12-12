d7bb744da34ed2d115c0bd6f001ba7f669ca7146
PhilKu7 <86799686+PhilKu7@users.noreply.github.com>
2023-12-12 12:08:51 +0100

    Refactor sound filtering and averaging logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include <Arduino.h>
 
-// Pin number for the LED
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 11
-#endif
-
 #define MOSFET_E0 10
 #define MOSFET_E1 7
 #define MOSFET_FAN 9
@@ -16,95 +11,55 @@
 #define BLUE MOSFET_E1
 #define MIC TEMP_0
 
-const int ledPin = LED_BUILTIN;
+// Number of samples to average
+const int numSamples = 10;
+
+// Circular buffer to store previous microphone readings
+int micBuffer[numSamples] = {0};
 
 void setup()
 {
   // Initialize serial communication
   Serial.begin(9600);
   // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
   pinMode(MIC, INPUT);
 }
 
-int sound = 0;
-int min, max, current_mic;
-
-// FIR filter coefficients
-const int filterLength = 5;
-const float filterCoefficients[filterLength] = {0.2, 0.2, 0.2, 0.2, 0.2};
-
-// Circular buffers to store previous microphone readings
-float maxBuffer[filterLength] = {0};
-float minBuffer[filterLength] = {0};
-float micBuffer[filterLength] = {0}; // Add this line to declare and initialize the micBuffer array
-
-// Variables to store filtered max and min values
-float filteredMax = 0;
-float filteredMin = 1024;
-
 void loop()
 {
-  max = 0;
-  min = 1024;
-  for (int i = 0; i < 10; i++)
+  int sound = 0;
+  int min = 1024;
+  int max = 0;
+  int current_mic;
+
+  for (int i = 0; i < numSamples; i++)
   {
     current_mic = analogRead(MIC);
-    if (current_mic < min)
-    {
-      min = current_mic;
-    }
-    if (current_mic > max)
-    {
-      max = current_mic;
-    }
+    min = min(current_mic, min);
+    max = max(current_mic, max);
     delay(2);
   }
 
-  // Apply filtering to max value
-  for (int i = filterLength - 1; i > 0; i--)
-  {
-    maxBuffer[i] = maxBuffer[i - 1];
-  }
-  maxBuffer[0] = max;
-
-  filteredMax = 0;
-  for (int i = 0; i < filterLength; i++)
-  {
-    filteredMax += filterCoefficients[i] * maxBuffer[i];
-  }
-
-  // Apply filtering to min value
-  for (int i = filterLength - 1; i > 0; i--)
-  {
-    minBuffer[i] = minBuffer[i - 1];
-  }
-  minBuffer[0] = min;
-
-  filteredMin = 1024;
-  for (int i = 0; i < filterLength; i++)
-  {
-    filteredMin += filterCoefficients[i] * minBuffer[i];
-  }
-
-  sound = filteredMax - filteredMin;
+  sound = max - min;
 
   // Update the circular buffer with the current sound value
-  for (int i = filterLength - 1; i > 0; i--)
+  for (int i = numSamples - 1; i > 0; i--)
   {
     micBuffer[i] = micBuffer[i - 1];
   }
   micBuffer[0] = sound;
 
-  // Apply the FIR filter
-  float filteredSound = 0;
-  for (int i = 0; i < filterLength; i++)
+  // Calculate the moving average of the sound values
+  int filteredSound = 0;
+  for (int i = 0; i < numSamples; i++)
   {
-    filteredSound += filterCoefficients[i] * micBuffer[i];
+    filteredSound += micBuffer[i];
   }
+  filteredSound /= numSamples;
 
   // Set RED pin to filtered sound value
   analogWrite(RED, filteredSound);   // 50% of 255
